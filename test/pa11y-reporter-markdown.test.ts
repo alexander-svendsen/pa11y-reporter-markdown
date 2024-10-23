353c29c92b176656dd024b3fb6df14c04d91ba59
alexander-svendsen <alexandersvendsen1990@gmail.com>
2024-10-23 18:34:54 +0200

    fix: cleanup tests

--- test/pa11y-reporter-markdown.test.ts
+++ test/pa11y-reporter-markdown.test.ts
@@ -7,10 +7,7 @@ const semver = require('semver')
 describe("pa11y-reporter-markdown", () => {
   it("must have the properties: supports, begin, error, debug, info, result", () => {
     expect(markdownReporter).toHaveProperty("supports")
-    expect(markdownReporter).toHaveProperty("begin")
     expect(markdownReporter).toHaveProperty("error")
-    expect(markdownReporter).toHaveProperty("debug")
-    expect(markdownReporter).toHaveProperty("info")
     expect(markdownReporter).toHaveProperty("results")
   });
 
